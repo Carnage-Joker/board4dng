74c9f7959b4efec015f492e65f7e302bd7a55fdc
Sam Sizlac <Sir_face92@outlook.com>
2024-08-25 22:40:23 +1000

    chore: Refactor manage.py and settings.py

--- messageboard/settings.py
+++ messageboard/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "board",
-    "board4dng",
 ]
 
 MIDDLEWARE = [
---
+++ messageboard/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # Make sure 'board.urls' is correctly defined
+    path('', include('board.urls')),
+]
+
+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+
