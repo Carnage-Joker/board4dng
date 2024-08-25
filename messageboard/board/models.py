09a7b175d512b1bfd622cd171dd4ced479127c04
Sam Sizlac <Sir_face92@outlook.com>
2024-08-25 15:35:58 +1000

    Save local changes before merge

--- messageboard/board/models.py
+++ messageboard/board/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, default='1')
+    author = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=User.objects.first)
 
     def __str__(self):
         return self.title
+
