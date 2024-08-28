2c0505ef0a3ae72711a6f3af08bfa2c39932d848
Sam Sizlac <Sir_face92@outlook.com>
2024-08-28 21:34:29 +1000

    Fix email settings again

--- messageboard/settings.py
+++ messageboard/settings.py
@@ -108,11 +108,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Ensure you have these lines set correctly:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.sendgrid.net')
-EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_PORT = config('EMAIL_PORT', default=465, cast=int)
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-DEFAULT_FROM_EMAIL = 'fease@digdeep.fitness'
+DEFAULT_FROM_EMAIL = 'info@thepinkbook.com.au'
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='apikey')
 EMAIL_USER = config('EMAIL_USER', default='apikey')
 
