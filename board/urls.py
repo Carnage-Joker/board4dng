02519ef0b7ac0c6a74ffe8974b449692b9129098
Sam Sizlac <Sir_face92@outlook.com>
2024-09-02 10:29:31 +1000

    Fix email configuratiohhhhhhhhjkkddddddddmkkmhn

--- board/urls.py
+++ board/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
 
-APP_NAME = 'board'
-
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('', views.user_login, name='login'),
+    path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('profile/<str:username>/', views.profile, name='profile'),
     path('messageboard/', views.message_board, name='message_board'),
@@ -15,6 +13,6 @@ urlpatterns = [
     path('approve/<int:post_id>/', views.approve_post, name='approve_post'),
     path('reject/<int:post_id>/', views.reject_post, name='reject_post'),
     path('flag/<int:post_id>/', views.flag_post, name='flag_post'),
-    path('create_message/', views.create_message,
-         name='create_message'),
+    path('create_message/', views.create_message, name='create_message'),
+    path('create_post/', views.create_post, name='create_post'),
 ]
