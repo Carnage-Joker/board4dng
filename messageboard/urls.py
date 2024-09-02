05b34e492e6821ed4a4aebecf8fdc87841d61207
Sam Sizlac <Sir_face92@outlook.com>
2024-09-02 11:25:26 +1000

    Refactor code to update URL configuration

--- messageboard/urls.py
+++ messageboard/urls.py
@@ -1,14 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Make sure 'board.urls' is correctly defined
     path('', include('board.urls')),
-
-    path('messageboard/', include('board.urls', namespace='board')),
+    path('messageboard/', include('board.urls')),
 ]
 
 static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
