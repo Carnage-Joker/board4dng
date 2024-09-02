3b2e0af1bcbdf6f1ace81e1ce083da5ac42e96a1
Sam Sizlac <Sir_face92@outlook.com>
2024-09-02 10:33:27 +1000

    dcd

--- board/urls.py
+++ board/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+APP_NAME = 'board'
+
+
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
