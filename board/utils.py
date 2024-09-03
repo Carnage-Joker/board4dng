484789946e833f49516ee142ad060cb02b33df51
Sam Sizlac <Sir_face92@outlook.com>
2024-09-03 11:41:07 +1000

    Fix SendGrid unauthorized error handling

--- board/utils.py
+++ board/utils.py
@@ -35,11 +35,8 @@ message = Mail(
     to_emails='phill.jones2112@gmail.com',
     subject='Sending with Twilio SendGrid is Fun',
     html_content='<strong>and easy to do anywhere, even with Python</strong>')
+sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
 try:
-    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
     response = sg.send(message)
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
 except Exception as e:
-    print(e.message)
+    print(f"Failed to send email: {str(e)}")
