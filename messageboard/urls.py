3d1ce05645f0739c66676e9eb250d937af2e2908
Sam Sizlac <Sir_face92@outlook.com>
2024-09-09 18:49:44 +1000

    gitttt

--- messageboard/urls.py
+++ messageboard/urls.py
@@ -5,11 +5,15 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', include('board.urls')),
+    # added a trailing slash for consistency
+    path('home/', include('board.urls')),
     path('messageboard/', include('board.urls')),
-    path('', include('pwa.urls')),
+    path('', include('pwa.urls')),  # PWA-related URLs
 ]
 
-static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
+# Only serve static and media files in development (debug mode)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
