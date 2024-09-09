5a2d271803c897748d75aa5c41ae131506e82a53
Sam Sizlac <Sir_face92@outlook.com>
2024-09-09 18:44:47 +1000

    hihg

--- messageboard/urls.py
+++ messageboard/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('board.urls')),
+    path('home', include('board.urls')),
     path('messageboard/', include('board.urls')),
     path('', include('pwa.urls')),
 ]
