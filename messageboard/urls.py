abc58840565bea23e3a5fa530a64db1130824f99
Carnajaytwa <82830372+Carnage-Joker@users.noreply.github.com>
2024-09-11 03:19:21 +1000

    Update urls.py

--- messageboard/urls.py
+++ messageboard/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
+from board import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
