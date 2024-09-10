d1c986d2b58f7d3c31029c2cdd568219ec546730
Carnajaytwa <82830372+Carnage-Joker@users.noreply.github.com>
2024-09-10 21:37:51 +1000

    Update urls.py

--- messageboard/urls.py
+++ messageboard/urls.py
@@ -5,11 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Explicit path for message board
+    path('', views.welcome, name='welcome'),
     path('messageboard/', include('board.urls')),
     path('pwa/', include('pwa.urls')),  # Explicit path for PWA URLs
 ]
-
 # Only serve static and media files in development (debug mode)
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
