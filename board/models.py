0d1eee722656a5860fe02b53172f83ecdbffaa8d
Sam Sizlac <Sir_face92@outlook.com>
2024-09-20 19:44:49 +1000

    newdb\1JddkkdSSShhiohklnhsmfnkfkjjnfJ

--- board/models.py
+++ board/models.py
@@ -46,7 +46,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['username', 'email']
+    REQUIRED_FIELDS = ['email']
 
     def __str__(self):
         return self.username
